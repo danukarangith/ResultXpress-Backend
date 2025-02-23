023f66f8b91a27b8ba3fd81c49fd15fbafb087ec
danukarangith <danukarangith456@gmail.com>
2025-02-23 23:20:52 +0530

    bug fix

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';  // Import cors
 import adminRoutes from './routes/AdminRoutes';
 import resultRoutes from './routes/ResultsRoutes';
 import studentRoutes from './routes/StudentRoutes';
+import {login} from "./controllers/AuthController";
+import authRoutes from "./routes/AuthRoutes";
 
 dotenv.config();
 
 const app = express();
+
+// Enable CORS for all origins
+app.use(cors());
+
+// If you want to allow only specific origins (e.g., your frontend's URL):
+// app.use(cors({ origin: 'http://localhost:3000' }));
+
 app.use(express.json());
 
+app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/results', resultRoutes);
 app.use('/api/students', studentRoutes);
+
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- src/routes/AdminRoutes.ts
+++ src/routes/AdminRoutes.ts
@@ -1,5 +1,12 @@
 import express from 'express';
-import {addResult, deleteResult, editResult, handleBulkUpload, uploadBulkResults} from '../controllers/AdminController';
+import {
+    addAdmin,
+    addResult, deleteAdmin,
+    deleteResult, editAdmin,
+    editResult,
+    handleBulkUpload,
+    uploadBulkResults
+} from '../controllers/AdminController';
 
 
 const router = express.Router();
@@ -8,5 +15,10 @@ router.post('/add-result', addResult);
 router.put('/edit-result/:id', editResult);
 router.delete('/delete-result/:id', deleteResult);
 router.post('/upload-results', uploadBulkResults, handleBulkUpload);
+router.post('/add-admin', addAdmin)
+router.put('/edit-admin/:id', editAdmin)
+router.delete('/delete-admin/:id', deleteAdmin)
+
+
 
 export default router;
